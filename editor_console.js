c6e077f392f1982c84027f0ec19ac571885e7d53
Võ Thị Xuân Quỳnh <euxcilia.quynhvo@gmail.com>
2024-12-08 14:04:23 +0700

    Update editor_console.js

--- editor_console.js
+++ editor_console.js
@@ -1,42 +1,77 @@
-class EditorConsole {
-  constructor() {
-    this.consoleLogList = document.getElementById('console-output');
-    this.consoleLogs = [];
-  }
+let console = (function (oldConsole) {
+    return {
+        formatArgsOutput: function(arg) { 
+            let outputArgMessage;
 
-  log(message, type = 'log') {
-    this.consoleLogs.push({ message, type });
-    this.printToConsole();
-  }
+            // Deal with different data types
+            switch (this.getType(arg)) {
+                case "string":
+                    outputArgMessage = `"${arg}"`;
+                    break;
+                case "object":
+                    outputArgMessage = `Object ${JSON.stringify(arg)}`;
+                    break;
+                case "array":
+                    outputArgMessage = `Array ${JSON.stringify(arg)}`;
+                    break;
+                default:
+                    outputArgMessage = arg;
+                    break;
+            }
 
-  clear() {
-    this.consoleLogs = [];
-    if (this.consoleLogList) {
-      this.consoleLogList.innerHTML = '';
+            return outputArgMessage;
+        },
+        getType: function (arg) {
+            if (typeof arg === "string") return "string";
+            if (typeof arg === "boolean") return "boolean";
+            if (typeof arg === "function") return "function";
+            if (typeof arg === "number") return "number";
+            if (typeof arg === "undefined") return "undefined";
+            if (typeof arg === "object" && !Array.isArray(arg)) return "object";
+            if (typeof arg === "object" && Array.isArray(arg)) return "array";
+        },
+        logMultipleArguments: function (arguments) {
+            let currentLog = "";
+
+            // Deal with multiple arguments
+            arguments.forEach(arg => {
+                currentLog += this.formatArgsOutput(arg) + " ";
+            });
+
+            oldConsole.log.apply(oldConsole, arguments);
+
+            consoleMessages.push({
+                message: currentLog,
+                class: `log log--default`
+            });
+
+            oldConsole.log(consoleMessages);
+        },
+        logSingleArgument: function (logItem) {
+            oldConsole.log(logItem);
+            consoleMessages.push({
+                message: this.formatArgsOutput(logItem),
+                class: `log log--${this.getType(logItem)}`
+            });
+
+            oldConsole.log(consoleMessages);
+        },
+        log: function (text) {
+            let argsArray = Array.from(arguments);
+            return argsArray.length !== 1 ? this.logMultipleArguments(argsArray) : this.logSingleArgument(text);
+        },
+        info: function (text) {
+            oldConsole.info(text);
+        },
+        warn: function (text) {
+            oldConsole.warn(text);
+        },
+        error: function (err) {
+            oldConsole.error(err);
+            consoleMessages.push({
+                message: `${err.name}: ${err.message}`,
+                class: "log log--error"
+            });
+        }
     }
-  }
-
-  printToConsole() {
-    this.consoleLogList.innerHTML = '';
-    this.consoleLogs.forEach(log => {
-      const logItem = document.createElement('li');
-      logItem.textContent = log.message;
-      logItem.className = log.type === 'error' ? 'log-error' : 'log-info';
-      this.consoleLogList.appendChild(logItem);
-    });
-  }
-}
-
-  async runPythonWithInput(pyodide, code) {
-    try {
-      await pyodide.runPythonAsync(code); // Chạy mã Python
-    } catch (err) {
-      // Log lỗi nếu xảy ra
-      this.log(`${err.name}: ${err.message}`, 'error');
-    }
-  }
-}
-
-// Xuất một đối tượng duy nhất
-const consoleHandler = new EditorConsole();
-export default consoleHandler;
+})(window.console);
